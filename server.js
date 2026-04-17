50e968a6c03dbd017c020b005d646103dfbefcab
andradanfahri-glitch <andradanfahri@gmail.com>
2026-04-17 14:25:15 +0700

    server.js

--- server.js
+++ server.js
@@ -6,23 +6,10 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 
-// ======================
-// TEST SERVER
-// ======================
 app.get("/", (req, res) => {
     res.send("Server aktif 🚀");
 });
 
-// ======================
-// INFO ROUTE
-// ======================
-app.get("/kirim", (req, res) => {
-    res.send("Gunakan POST untuk kirim data");
-});
-
-// ======================
-// TEST WHATSAPP
-// ======================
 app.get("/test-wa", async (req, res) => {
     try {
         const result = await axios.post("https://api.fonnte.com/send", {
@@ -37,14 +24,10 @@ app.get("/test-wa", async (req, res) => {
         res.json(result.data);
 
     } catch (error) {
-        console.log(error.response?.data || error.message);
         res.json(error.response?.data || error.message);
     }
 });
 
-// ======================
-// KIRIM ABSENSI + GPS
-// ======================
 app.post("/kirim", async (req, res) => {
     const { nama, nip, foto, lokasi } = req.body;
 
@@ -53,13 +36,15 @@ app.post("/kirim", async (req, res) => {
 Absensi Karyawan
 Nama: ${nama}
 NIP: ${nip}
-Lokasi: ${lokasi || "Tidak tersedia"}
+Lokasi: ${lokasi || "-"}
         `;
 
         await axios.post("https://api.fonnte.com/send", {
             target: "6285784117242",
             message: pesan,
-            image: foto
+            image: foto.startsWith("data:image")
+                ? foto
+                : `data:image/jpeg;base64,${foto}`
         }, {
             headers: {
                 Authorization: "dNnddRHMv4BUGoXnbLFY"
@@ -76,8 +61,5 @@ Lokasi: ${lokasi || "Tidak tersedia"}
     }
 });
 
-// ======================
-// PORT
-// ======================
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server jalan 🚀"));
